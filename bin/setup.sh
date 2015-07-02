9c8fa3d64358960c190d8b6a2fdcd66c64100c18
Kamal Joshi <kamal@hackerrank.com>
2015-07-03 02:05:07 +0530

    Assume yes.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,17 +1,17 @@
 case `uname -a` in
 Linux*x86_64*)  echo "Linux 64 bit"
     sudo apt-get update
-    sudo apt-get install nodejs
-    sudo apt-get install npm
-    sudo apt-get install libxml2-dev
+    sudo apt-get --assume-yes install nodejs
+    sudo apt-get --assume-yes install npm
+    sudo apt-get --assume-yes install libxml2-dev
     sudo ln -s "$(which nodejs)" /usr/bin/node || true
     ;;
 
 Linux*i686*)  echo "Linux 32 bit"
     sudo apt-get update
-    sudo apt-get install nodejs
-    sudo apt-get install npm
-    sudo apt-get install libxml2-dev
+    sudo apt-get --assume-yes install nodejs
+    sudo apt-get --assume-yes install npm
+    sudo apt-get --assume-yes install libxml2-dev
     sudo ln -s "$(which nodejs)" /usr/bin/node || true
     ;;
 
